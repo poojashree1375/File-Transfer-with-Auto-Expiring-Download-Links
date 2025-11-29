d55274cc287605fbf89a870d24facba59788635d
poojashree1375 <poojashreee13725@gmail.com>
2025-11-29 18:30:45 +0530

    Added FileUpload Model

--- uploader/models.py
+++ uploader/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class FileUpload(models.Model):
+    token=models.CharField(max_length=100,unique=True,default=uuid.uuid4)
+    expiry_datetime=models.DateTimeField()
+    uploaded_at=models.DateTimeField(auto_now_add=True)
+    file_name=models.CharField(max_length=255)
+    file_type=models.CharField(max_length=100)
+    encrypted_data=models.BinaryField()
+
+    def __str__(self):
+        return f"EncryptedFile(token={self.token})"
+    
